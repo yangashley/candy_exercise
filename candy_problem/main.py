72041562f5a1ba785b06902f62d57ad1adfc244b
Audrey Andoy <audreylandoy@gmail.com>
2021-09-13 16:32:29 -0700

    candy starter

---
+++ candy_problem/main.py
@@ -0,0 +1,18 @@
+'''
+Given the list `friend_favorites` create 
+a new data structure that describes different 
+types of candy with a list of friends that 
+like that particular candy.
+'''
+
+friend_favorites = [
+    ["sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+    [ "Bob", ["milky way", "licorice", "lollipop"]],
+	[ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+	[ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+]
+
+
+def create_new_candy_data_structure():
+    return "hi"
+
---
+++ tests/test_candy_data_structure.py
@@ -0,0 +1,6 @@
+import pytest
+from candy_problem.main import * 
+
+def test_create_candy_data_structure():
+    pass
+    
