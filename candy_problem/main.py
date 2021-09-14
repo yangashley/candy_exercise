55f789ec093300e7bbb15bee7427c8c38655f3d5
Audrey Andoy <audreylandoy@gmail.com>
2021-09-14 07:30:38 -0700

    changed function name for get friends who like specific candy and fixed spacing for Sally

--- candy_problem/main.py
+++ candy_problem/main.py
@@ -8,13 +8,13 @@ that describes the different types of candy paired with a list of friends that
 like that candy. 
 
 friend_favorites = [
-    ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
+    [ "Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
     [ "Bob", ["milky way", "licorice", "lollipop"]],
 	[ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
 	[ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
 ]
 
-2. In `get_friends_who_like_candy_type()`, return friends who like lollipops.
+2. In `get_friends_who_like_specific_candy()`, return friends who like lollipops.
 
 3. In, `create_candy_set()`, return a set of all the candies from
 the data structure made in `create_new_candy_data_structure()`.
@@ -29,7 +29,7 @@ def create_new_candy_data_structure(data):
     pass 
 
 
-def get_friends_who_like_candy_type(data, type):
+def get_friends_who_like_specific_candy(data, candy_name):
     pass
 
 def create_candy_set(data):
