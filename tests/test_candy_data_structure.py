7f06cb4a02d217d0ad75d7519e21ddecac765e46
Audrey Andoy <audreylandoy@gmail.com>
2021-09-13 17:17:03 -0700

    added original data structure to test

--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -3,4 +3,17 @@ from candy_problem.main import *
 
 def test_create_candy_data_structure():
     pass
+
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    # Act
+    new_candy_data = create_new_candy_data_structure(friend_favorites)
+    
+    # Assert
     
