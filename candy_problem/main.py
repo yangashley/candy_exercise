cbd85716e2d0021ddcfe5c2cfeb1c25c52687d20
Audrey Andoy <audreylandoy@gmail.com>
2021-09-13 18:01:59 -0700

    changed function name to have students get list of friends who like a particular candy type instead

--- candy_problem/main.py
+++ candy_problem/main.py
@@ -7,8 +7,7 @@ a new data structure in the function `create_new_candy_data_structure`
 that describes the different types of candy paired with a list of friends that 
 like that candy. 
 
-2. In `get_favorite_candy_by_name()`, return the candies
-that Arlene likes.
+2. In `get_friends_who_like_candy_type()`, return friends who like lollipops.
 
 3. In, `create_candy_set()`, return a set of all the candies from
 the data structure made in `create_new_candy_data_structure()`.
@@ -29,10 +28,8 @@ def create_new_candy_data_structure(data):
     pass 
 
 
-
-def get_favorite_candy_by_name(data, name):
-    pass 
-    
+def get_friends_who_like_candy_type(data, type):
+    pass
 
 def create_candy_set(data):
     pass 
