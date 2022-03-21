e61885d53cd447002199de7bab6cf0599490f5b2
Audrey Andoy <audreylandoy@gmail.com>
2022-03-21 10:18:29 -0700

    added tuples for problem 2

--- candy_problem/main.py
+++ candy_problem/main.py
@@ -14,7 +14,7 @@ friend_favorites = [
 	[ "Carlie", [ "nerds", "sour patch kids", "laffy taffy" ]]
 ]
 
-2. In `get_friends_who_like_specific_candy()`, return friends who like lollipops.
+2. In `get_friends_who_like_specific_candy()`, return a tuple of friends who like lollipops.
 
 3. In, `create_candy_set()`, return a set of all the candies from
 the data structure made in `create_new_candy_data_structure()`.
