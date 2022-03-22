1244d067e5ac388c5b7be60d6eb11bce6ebd4b17
Audrey Andoy <audreylandoy@gmail.com>
2022-03-21 22:36:52 -0700

    updated main with C17 changes
    Added problem to do as a class, wordsmithed, removed extra whitespace, provided more guidance on tests.

--- candy_problem/main.py
+++ candy_problem/main.py
@@ -2,35 +2,50 @@
 DIRECTIONS
 ==========
 
-1. Given the list `friend_favorites`, create 
+1. In `get_friends_most_favored_candy()`, return a dictionary of candy names and the
+amount of times each candy appears in the `friend_favorites` list.
+
+friend_favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+]
+
+2. Given the list `friend_favorites`, create 
 a new data structure in the function `create_new_candy_data_structure` 
 that describes the different kinds of candy paired with a list of friends that 
 like that candy. 
 
 friend_favorites = [
-    [ "Sally", [ "lollipop", "bubble gum", "laffy taffy" ]],
-    [ "Bob", [ "milky way", "licorice", "lollipop" ]],
-	[ "Arlene", [ "chocolate bar", "milky way", "laffy taffy" ]],
-	[ "Carlie", [ "nerds", "sour patch kids", "laffy taffy" ]]
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
 ]
 
-2. In `get_friends_who_like_specific_candy()`, return a tuple of friends who like lollipops.
+3. In `get_friends_who_like_specified_candy()`, return 
+a tuple of friends who like the candy specified in the candy_name parameter.
 
-3. In, `create_candy_set()`, return a set of all the candies from
+4. In, `create_candy_set()`, return a set of all the candies from
 the data structure made in `create_new_candy_data_structure()`.
 
-4. Write tests for all of the functions in this exercise.
-
+5. Starting with nominal cases, write tests for each of the functions below then 
+write tests to handle edge cases.
 '''
 
+#1
+def get_friends_most_favored_candy(favorites):
+    pass
 
+#2
 def create_new_candy_data_structure(data):
     pass 
 
-
+#3
 def get_friends_who_like_specific_candy(data, candy_name):
     pass
 
-
+#4
 def create_candy_set(data):
     pass 
